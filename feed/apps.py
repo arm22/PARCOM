1b300b0bf5d5c411693780feac46d7f33e5a2cde
Austin Meyers <meyersaustin22@gmail.com>
2016-05-17 11:16:04 -0700

    Get feed working

--- feed/apps.py
+++ feed/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
 
-
 class FeedConfig(AppConfig):
     name = 'feed'
--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Study
 
 # Create your views here.
+def study_list(request):
+    return render(request, 'feed/study_list.html', {})
